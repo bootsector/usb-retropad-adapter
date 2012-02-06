e1acb2fa727e1fac50debaf342178d43d495aaff
bootsector <bootsector@ig.com.br>
2012-02-06 20:12:52 -0200

    Stopped using Bill Porter's PSX/PS2 lib because it relies on Arduino timer functions. Now using a much more simplified, yet VERY functional library, that don't make use of millis(), micros() or delay() functions.

---
+++ src/psx.cpp
@@ -0,0 +1,151 @@
+/*
+ * USB RetroPad Adapter - PC/PS3 USB adapter for retro-controllers!
+ * Copyright (c) 2012 Bruno Freitas - bootsector@ig.com.br
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <WProgram.h>
+#include "digitalWriteFast.h"
+#include "psx.h"
+
+psxpad_state_t pad_state;
+
+void psx_init() {
+	pinModeFast(DAT, INPUT);
+	pinModeFast(CMD, OUTPUT);
+	pinModeFast(ATT, OUTPUT);
+	pinModeFast(CLK, OUTPUT);
+
+	digitalWriteFast(DAT, HIGH);
+	digitalWriteFast(CMD, HIGH);
+	digitalWriteFast(ATT, HIGH);
+	digitalWriteFast(CLK, HIGH);
+
+	memset(&pad_state, 0x00, sizeof(psxpad_state_t));
+	pad_state.l_x_axis = 0x80;
+	pad_state.l_y_axis = 0x80;
+	pad_state.r_x_axis = 0x80;
+	pad_state.r_y_axis = 0x80;
+}
+
+void psx_wait_ack() {
+	delayMicroseconds(50);
+}
+
+byte psx_command(byte command) {
+	byte i = 0;
+	byte data = 0;
+
+	delayMicroseconds(50);
+
+	for (i = 0; i < 8; i++) {
+		if (command & 1)
+			digitalWriteFast(CMD, HIGH);
+		else
+			digitalWriteFast(CMD, LOW);
+
+		command >>= 1;
+
+		digitalWriteFast(CLK, LOW);
+
+		delayMicroseconds(PSXDELAY);
+
+		data >>= 1;
+
+		digitalWriteFast(CLK, HIGH);
+
+		delayMicroseconds(PSXDELAY - 1);
+
+		if (digitalReadFast(DAT))
+			data |= (1 << 7); //(1<<i);
+	}
+
+	delayMicroseconds(CTRL_BYTE_DELAY);
+
+	return data;
+}
+
+psxpad_state_t* psx_read(byte* pad_id) {
+	byte data, id;
+
+	digitalWriteFast(ATT, LOW);
+	delayMicroseconds(1);
+
+	data = psx_command(0x01); // Issue start command, data is discarded
+
+	psx_wait_ack();
+
+	id = psx_command(0x42); // Request controller ID
+
+	*pad_id = id;
+
+	if ((id == PSX_ID_DIGITAL) | (id == PSX_ID_A_RED)
+			| (id == PSX_ID_A_GREEN)) {
+
+		data = psx_command(0xff); // expect 0x5a from controller
+
+		if (data == 0x5a) {
+
+			data = psx_command(0xff);
+
+			pad_state.select_btn = !(data & (1 << 0));
+			pad_state.start_btn = !(data & (1 << 3));
+
+			if (id == PSX_ID_DIGITAL) {
+				pad_state.up_btn = !(data & (1 << 4));
+				pad_state.down_btn = !(data & (1 << 6));
+				pad_state.left_btn = !(data & (1 << 7));
+				pad_state.right_btn = !(data & (1 << 5));
+			} else if (id == PSX_ID_A_RED) {
+				pad_state.l3_btn = !(data & (1 << 1));
+				pad_state.r3_btn = !(data & (1 << 2));
+
+				pad_state.up_btn = !(data & (1 << 4));
+				pad_state.down_btn = !(data & (1 << 6));
+				pad_state.left_btn = !(data & (1 << 7));
+				pad_state.right_btn = !(data & (1 << 5));
+			}
+
+			data = psx_command(0xff);
+
+			pad_state.l2_btn = !(data & (1 << 0));
+			pad_state.r2_btn = !(data & (1 << 1));
+			pad_state.l1_btn = !(data & (1 << 2));
+			pad_state.r1_btn = !(data & (1 << 3));
+			pad_state.triangle_btn = !(data & (1 << 4));
+			pad_state.circle_btn = !(data & (1 << 5));
+			pad_state.cross_btn = !(data & (1 << 6));
+			pad_state.square_btn = !(data & (1 << 7));
+
+			if ((id == PSX_ID_A_RED) | (id == PSX_ID_A_GREEN)) {
+				data = psx_command(0xff);
+				pad_state.r_x_axis = data;
+
+				data = psx_command(0xff);
+				pad_state.r_y_axis = data;
+
+				data = psx_command(0xff);
+				pad_state.l_x_axis = data;
+
+				data = psx_command(0xff);
+				pad_state.l_y_axis = data;
+			}
+		}
+	}
+
+	digitalWriteFast(ATT, HIGH);
+
+	return &pad_state;
+}
--- src/usbra.cpp
+++ src/usbra.cpp
@@ -18,11 +18,11 @@
 
 #include <WProgram.h>
 #include "USBVirtuaStick.h"
-#include "PS2X_lib.h"
 #include "genesis.h"
 #include "NESPad.h"
 #include "GCPad_16Mhz.h"
 #include "digitalWriteFast.h"
+#include "psx.h"
 
 // Arcade mode and extension cable detection pins
 #define ARCADE_DB9_PIN	12
@@ -272,70 +272,69 @@ void snes_loop() {
 }
 
 void ps2_loop() {
-	PS2X psPad;
+	psxpad_state_t *psPad;
 	byte dir = 0;
+	byte pad_id;
 
-	//while (psPad.config_gamepad(8, 6, 7, 5, false, false) == 1);
-
-	psPad.config_gamepad(8, 6, 7, 5, false, false);
+	psx_init();
 
 	for (;;) {
 		//vs_reset_watchdog();
 
-		psPad.read_gamepad();
+		psPad = psx_read(&pad_id);
 
-		if(psPad.readType() == 0) {
+		if(pad_id == PSX_ID_DIGITAL) {
 			gamepad_state.l_x_axis = 0x80;
 			gamepad_state.l_y_axis = 0x80;
 
-			if(psPad.Button(PSB_PAD_LEFT)) {
+			if(psPad->left_btn) {
 				gamepad_state.l_x_axis = 0x00;
-			} else if (psPad.Button(PSB_PAD_RIGHT)) {
+			} else if (psPad->right_btn) {
 				gamepad_state.l_x_axis = 0xFF;
 			}
 
-			if(psPad.Button(PSB_PAD_UP)) {
+			if(psPad->up_btn) {
 				gamepad_state.l_y_axis = 0x00;
-			} else if (psPad.Button(PSB_PAD_DOWN)) {
+			} else if (psPad->down_btn) {
 				gamepad_state.l_y_axis = 0xFF;
 			}
 
 		} else {
-			gamepad_state.l_x_axis = psPad.Analog(PSS_LX);
-			gamepad_state.l_y_axis = psPad.Analog(PSS_LY);
-			gamepad_state.r_x_axis = psPad.Analog(PSS_RX);
-			gamepad_state.r_y_axis = psPad.Analog(PSS_RY);
+			gamepad_state.l_x_axis = psPad->l_x_axis;
+			gamepad_state.l_y_axis = psPad->l_y_axis;
+			gamepad_state.r_x_axis = psPad->r_x_axis;
+			gamepad_state.r_y_axis = psPad->r_y_axis;
 
-			dir = psPad.Button(PSB_PAD_UP) << 3 | psPad.Button(PSB_PAD_DOWN) << 2 | psPad.Button(PSB_PAD_LEFT) << 1 | psPad.Button(PSB_PAD_RIGHT);
+			dir = psPad->up_btn << 3 | psPad->down_btn << 2 | psPad->left_btn << 1 | psPad->right_btn;
 
 			gamepad_state.direction = pad_dir[dir];
 		}
 
-		gamepad_state.square_btn = psPad.Button(PSB_SQUARE);
+		gamepad_state.square_btn = psPad->square_btn;
 
-		gamepad_state.cross_btn = psPad.Button(PSB_CROSS);
+		gamepad_state.cross_btn = psPad->cross_btn;
 
-		gamepad_state.circle_btn = psPad.Button(PSB_CIRCLE);
+		gamepad_state.circle_btn = psPad->circle_btn;
 
-		gamepad_state.l1_btn =psPad.Button(PSB_L1);
+		gamepad_state.l1_btn = psPad->l1_btn;
 
-		gamepad_state.l2_btn =psPad.Button(PSB_L2);
+		gamepad_state.l2_btn = psPad->l2_btn;
 
-		gamepad_state.triangle_btn = psPad.Button(PSB_TRIANGLE);
+		gamepad_state.triangle_btn = psPad->triangle_btn;
 
-		gamepad_state.r1_btn = psPad.Button(PSB_R1);
+		gamepad_state.r1_btn = psPad->r1_btn;
 
-		gamepad_state.r2_btn = psPad.Button(PSB_R2);
+		gamepad_state.r2_btn = psPad->r2_btn;
 
-		gamepad_state.l3_btn = psPad.Button(PSB_L3);
+		gamepad_state.l3_btn = psPad->l3_btn;
 
-		gamepad_state.r3_btn = psPad.Button(PSB_R3);
+		gamepad_state.r3_btn = psPad->r3_btn;
 
-		gamepad_state.select_btn = psPad.Button(PSB_SELECT);
+		gamepad_state.select_btn = psPad->select_btn;
 
-		gamepad_state.start_btn = psPad.Button(PSB_START);
+		gamepad_state.start_btn = psPad->start_btn;
 
-		gamepad_state.ps_btn = psPad.Button(PSB_SELECT) && psPad.Button(PSB_START); // SELECT + START = PS Button
+		gamepad_state.ps_btn = psPad->select_btn && psPad->start_btn; // SELECT + START = PS Button
 
 		vs_send_pad_state();
 	}
